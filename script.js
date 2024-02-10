39066b0e029a37cf6287cc0e07b85986c8cac59e
Thulio Queiroz <64506302+Thu1z@users.noreply.github.com>
2024-02-09 21:03:32 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function enviar(){
+    let nome = document.querySelector('input[type="text"]').value;
+    let email = document.querySelector('input[type="email"]').value;
+    alert("nome: " + nome + "\n email: " + email);
+
+}
